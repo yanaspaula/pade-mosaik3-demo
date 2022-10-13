7a060d4ee839e2ef1e9faaae629ba1c475a6c680
yanaspaula <yanaspaula@gmail.com>
2022-10-13 13:11:51 -0300

    dúvidas e ajustes em "device_agent.py"

--- device_agent.py
+++ device_agent.py
@@ -14,8 +14,10 @@ import pickle
 import random
 
 MOSAIK_MODELS = {
+    'api_version': '3.0', # DOCUMENTATION: Since mosaik API version 2.2, the simulator’s major version (“x”, in the snippet above) has to be equal to mosaik’s. Mosaik will cancel the simulation if a version mismatch occurs.
     'type': 'event-based', # Mosaik 3.0: Component's type
-    'api_version': '2.2',
+    # DOCUMENTATION: Time-based simulators only decide themselves on which points in time they want to be stepped (i.e. communicate with the other simulators). Their output is valid until the next step.
+    # DOCUMENTATION: Event-based simulators are always stepped when a predecessor provides new input, but they can also schedule steps for themselves. 
     'models': {
         'DeviceAgent': {
             'public': True,
@@ -26,9 +28,9 @@ MOSAIK_MODELS = {
 }
 
 
-class MosaikSim(MosaikCon):
+class MosaikSim(MosaikCon): # Dúvida: onde posso acessar a classe mosaik_api.Simulator para comparar com a classe mosaik_driver.MosaikCon?
 
-    def __init__(self, agent):
+    def __init__(self, agent): # Dúvida: onde está o step_size? No mosaik_driver -> posso retirar?
         super(MosaikSim, self).__init__(MOSAIK_MODELS, agent)
         self.prosumer_sim_prefix = 'ProsumerSim0-0.Prosumer_'
         self.prosumer_data = {'stochastic_gen': [],
@@ -41,13 +43,14 @@ class MosaikSim(MosaikCon):
 
     # def init(self, sid, eid_prefix, prosumer_ref, start, step_size): # Mosaik 2.0
     def init(self, sid, time_resolution, eid_prefix, prosumer_ref, start): # Mosaik 3.0: Time resolution
+        # Dúvida: o que fazer com o time_resolution? Como tratá-lo???
         # self.sid = sid
         self.prosumer_ref = 'ProsumerSim0-0.Prosumer_{}'.format(prosumer_ref)
         self.node_id = prosumer_ref
         self.eid_prefix = eid_prefix
         self.eid = '{}-{}'.format(self.eid_prefix, prosumer_ref)
         self.start = start
-        # self.step_size = step_size # Mosaik 2.0
+        # self.step_size = step_size # Mosaik 2.0       # Dúvida: como refletir no mosaik_driver?
         return MOSAIK_MODELS
 
 
@@ -77,7 +80,7 @@ class MosaikSim(MosaikCon):
         }
         '''
         # print(inputs)
-        if time % (1 * 60) == 0 and time != 0: # a cada 5 min
+        if time % (1 * 60) == 0 and time != 0: # a cada 5 min # Dúvida: o que fazer com isso????
             # =================================================
             # armazenamento dos parâmetros vindos do Mosaik
             # No momento apenas: staus e demanda.
@@ -106,7 +109,7 @@ class MosaikSim(MosaikCon):
             pass
 
         #return time + self.step_size # Mosaik 2.0
-        return None # Mosaik 3.0
+        return None # Mosaik 3.0            # Dúvida: como refletir no mosaik_driver?
 
     def handle_set_data(self):
         pass
